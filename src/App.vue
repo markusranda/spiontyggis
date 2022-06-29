b77db9f53429f323245c6c80f12f971c2973b21b
markusranda <randamarkus.95@gmail.com>
2022-06-29 16:02:41 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <img src="./assets/spy.png" alt="" />
+    <h1>Spiontyggis.no</h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+header {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+header h1 {
+  font-family: "Special Elite", cursive;
+  color: black;
+  font-size: 2rem;
+}
+
+img {
+  height: 4rem;
+}
+</style>
